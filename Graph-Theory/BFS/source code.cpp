19b43a25c3473a8e1ac02345b6b8257f58671f5d
Md. Abul Kalam Azad <akazad.cse13@gmail.com>
2018-01-07 02:42:44 +0600

    Update source code.cpp

--- Graph-Theory/BFS/source code.cpp
+++ Graph-Theory/BFS/source code.cpp
@@ -1,88 +1,53 @@
-/* A K Azad
-     CSE-13
-     RUET.
-
-*/
-
-
-//Directed Graph............................
-
 #include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
 
-#define max 10000
-vector<int> edge[max];
-int parent[max];
+#define Max 10000 // node limit
+#define Clear(a,b) memset(a,b,sizeof a)
+#define si(a) scanf("%d",&a)
+#define sl(a) scanf("%lld",&a)
 
-void bfs(int n,int src)
+vector<int> g[Max];
+int parent[Max];
+
+void bfs(int src, int n)
 {
-	queue<int>Q;
+    int vis[Max],level[Max]; // level = distance from source
+    Clear(vis,0);
+    queue<int>Q;
 	Q.push(src);
-	int visited[max]={0},level[max];
-	visited[src]=1;
+
+	vis[src]=1;
     level[src]=0;
 	while(!Q.empty())
 	{
-		int u=Q.front();
-		for(int i=0;i<edge[u].size();i++)
+		int u=Q.front(); Q.pop();
+		int len = g[u].size();
+		for(int i=0;i<len;i++)
 		{
-			int v=edge[u][i];
-			if(!visited[v])
+			int v=g[u][i];
+			if(!vis[v])
 			{
-			    cout<<v<<endl;
 				level[v]=level[u]+1;
 				parent[v]=u;
-				visited[v]=1;
+				vis[v]=1;
 				Q.push(v);
 			}
 		}
-		Q.pop();
 	}
-	for(int i=1;i<=n;i++)
-		printf("%d to %d distance %d\n",src,i,level[i]);
 }
-
-
-
 int main()
 {
-    freopen("input1.txt","r",stdin);
+    int n,e,u,v; // n=node, e=edge
+    si(n); si(e);
 
-
-    int in[max]={0};
-    int out[max]={0};
-
-    int N,E;
-
-    cin>>N>>E;
-
-    for(int i=1;i<=E;i++)
+    for(int i=1;i<=e;i++)
     {
-        int n1,n2;
-        cin>>n1>>n2;
-        edge[n1].push_back(n2);
+        si(u); si(v);
+        g[u].push_back(v);
+        g[v].push_back(u); // for bidirectional
     }
-
-    bfs(N,1);
-
-    printf("Enter a node number to find its path from source: ");
-    int node;
-    while(cin>>node,node)
-    {
-        cout<<"node "<<node<<":";
-        while(parent[node]>=1)
-        {
-            cout<<" "<<parent[node];
-            node  = parent[node];
-        }
-        cout<<"\n";
-    }
-
+    bfs(1,n);
     return 0;
-
 }
-
-
-
