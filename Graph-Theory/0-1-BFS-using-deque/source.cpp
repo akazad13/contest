675a84dbccb010904d15b46df7d46490ed3196e7
Md. Abul Kalam Azad <akazad.cse13@gmail.com>
2018-01-07 12:34:25 +0600

    Update source.cpp

--- Graph-Theory/0-1-BFS-using-deque/source.cpp
+++ Graph-Theory/0-1-BFS-using-deque/source.cpp
@@ -1,29 +1,38 @@
-
-
 // Can be used a graph where each of the node weights 0 or x
-
 #include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
 
+#define Max 1010// node limit
+#define Clear(a,b) memset(a,b,sizeof a)
+#define si(a) scanf("%d",&a)
+#define sl(a) scanf("%lld",&a)
+#define ll long long int
+
 int n,m;
-int cost[1010][1010];
-char g[1010][1010];
+ll cost[Max][Max];
+char g[Max][Max];
 
 int fx[]={1,-1,0,0};
 int fy[]={0,0,1,-1};
 
 #define valid(i,j) i>=0 && i<n && j>=0 && j<m
 
-void bfs()
+ll zero_one_bfs()
 {
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            cost[i][j]=1000000000000000;
+        }
+    }
     deque< pair< int,int> > deq;
-
     pair<int,int> p;
 
     p = make_pair(0,0);
-    cost[0][0]=0;
+    cost[0][0]=0;  // start at (0,0).... i can change that
     deq.push_front(p);
 
     while(!deq.empty())
@@ -33,8 +42,8 @@ void bfs()
         int x = p.first;
         int y = p.second;
 
-        if(x==n-1 && y==m-1)
-            return ;
+        if(x==n-1 && y==m-1)  // destination at (n-1,m-1).. can change that
+            return cost[x][y];
         for(int i=0;i<4;i++)
         {
             int xx = x+fx[i];
@@ -42,18 +51,18 @@ void bfs()
 
             if(valid(xx,yy))
             {
-                if(g[x][y]==g[xx][yy])
+                if(g[x][y]==g[xx][yy]) // if same than 0 cost here .. 0 cost first
                 {
                     if(cost[x][y]<cost[xx][yy])
                     {
-                        cost[xx][yy]=cost[x][y];
+                        cost[xx][yy]=cost[x][y]+0;
                         p = make_pair(xx,yy);
                         deq.push_front(p);
                     }
                 }
                 else
                 {
-                    if(cost[x][y]+1<cost[xx][yy])
+                    if(cost[x][y]+1<cost[xx][yy])  // not same , k cost added
                     {
                         cost[xx][yy]=cost[x][y]+1;
                         p = make_pair(xx,yy);
@@ -64,37 +73,27 @@ void bfs()
             }
         }
     }
+    return -1;
 }
 
 int main()
 {
-    //freopen("inn.txt","r",stdin);
     int test;
     char ch;
-    cin>>test;
+    si(test);
 
     for(int Case=1; Case<=test; Case++)
     {
-        scanf("%d %d",&n,&m);
+        si(n); si(m);
         ch = getchar();
         for(int i=0;i<n;i++)
         {
-           // cin>>g[i];
            scanf("%s",&g[i]);
         }
 
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                cost[i][j]=INFINITY;
-            }
-        }
-      //  cout<<cost[n-1][m-1]<<endl;
-        bfs();
-        printf("%d\n",cost[n-1][m-1]);
+
+        zero_one_bfs();
+        printf("Case %d: %lld\n",Case,cost[n-1][m-1]);
     }
-
-
     return 0;
 }
