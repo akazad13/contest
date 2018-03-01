d8557022ca81a02453aa63bee5b850fc733f7b4e
Md. Abul Kalam Azad <akazad.cse13@gmail.com>
2018-03-02 02:31:55 +0600

    Update source code.cpp

--- Number-theory/Euler-PI-function-to-determine-relative-prime/source code.cpp
+++ Number-theory/Euler-PI-function-to-determine-relative-prime/source code.cpp
@@ -1,3 +1,8 @@
+#define MAX 10000010
+bool prime[MAX+10];
+vector<int>primeList; //for saving prime number in a vector
+int Vsize;
+
 template<typename T>inline T POW(T B,T P)
 {
     if(P==0) return 1;
@@ -5,10 +10,44 @@ template<typename T>inline T POW(T B,T P)
     else return SQR(POW(B,P/2));
 }
 
+void seive()
+{
+    //generating prime table
+    for(int i=4; i<=MAX; i+=2)
+        prime[i]=1;
+    int srt = sqrt(MAX);
+    for(int i=3; i<=srt; i+=2)
+    {
+        if(prime[i]==0)
+        {
+            for(int j=i*i; j<=MAX; j+=2*i)
+            {
+                prime[j]=1;
+            }
+        }
+    }
+    prime[0]=prime[1]=1;
+
+
+    //for saving the prime in one place
+    for(int i=2; i<=MAX; i++)
+    {
+        if(prime[i]==0)
+        {
+            primeList.push_back(i);
+        }
+    }
+
+    Vsize = primeList.size();
+
+    // cout<<"seive OK"<<endl;
+}
+
+
 // PList is the prime List
 // prime is the sieve
 
-int PI_f(int m)
+ll PI_f(int m)
 {
     if(prime[m]==0)
     {
@@ -16,19 +55,19 @@ int PI_f(int m)
     }
     ll res=1;
 
-    for(int i=0;i<Vsize && pList[i]*pList[i]<=m;i++)
+    for(int i=0; i<Vsize && primeList[i]*primeList[i]<=m; i++)
     {
         if(m<=1)
             break;
-        if(m%pList[i]==0)
+        if(m%primeList[i]==0)
         {
-            int cnt=0;
-            while(m%pList[i]==0)
+            ll cnt=0;
+            while(m%primeList[i]==0)
             {
-                m/=pList[i];
+                m/=primeList[i];
                 cnt++;
             }
-            res = res*(pList[i]-1)*round(pow(pList[i],cnt-1)); // PI(n) = (p1^(a1-1))*(p1-1)*(p2^(a2-1))*(p2-1)*...
+            res = res*(primeList[i]-1)*round(pow(primeList[i],cnt-1)); // PI(n) = (p1^(a1-1))*(p1-1)*(p2^(a2-1))*(p2-1)*...
         }
         if(prime[m]==0 && m>1)
         {
