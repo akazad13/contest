58c456188ac64c233fcc9b680f1cf65ddaa84110
Md. Abul Kalam Azad <akazad.cse13@gmail.com>
2018-05-08 00:04:10 +0600

    Update 2D segmented tree.cpp

--- Data-Structure/Segment-Tree/2D segmented tree.cpp
+++ Data-Structure/Segment-Tree/2D segmented tree.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<bits/stdc++.h>
 
@@ -6,15 +5,6 @@ using namespace std;
 
 #define rep(i,p,n) for( i = p; i<n;i++)
 #define lld long long int
-
-#define pb push_back
-#define VI vector<int>
-#define VL vector<long long int>
-#define VD vector<double>
-
-#define pi pair<int,int>
-#define mp(a,b) make_pair(a,b)
-
 #define Clear(a,b) memset(a,b,sizeof(a))
 
 #define Max 505
@@ -31,27 +21,18 @@ template<class T>inline bool read(T &x) {
     return ~c;
 }
 
-/*struct node
-{
-    //int x,y;
-    int val;
-};*/
 
 int arr[Max][Max];
-//node tree[3*Max*Max];
 int tree[4*Max*Max+4];
 
 
 
-void tree_build(int node, int a1, int b1, int a2, int b2)
+void build(int node, int a1, int b1, int a2, int b2)
 {
-   // cout<<node<<endl;
     if (a1 > a2 or b1 > b2)
         return ;
     if(a1==a2 and b1==b2)
     {
-        //tree[node].x = a1;
-       // tree[node].y = b1;
         tree[node] = arr[a1][b1];
         return ;
     }
@@ -59,17 +40,17 @@ void tree_build(int node, int a1, int b1, int a2, int b2)
     int mid1 = (a1+a2)/2;
     int mid2 = (b1+b2)/2;
 
-    tree_build(4*node-2,a1,b1,mid1,mid2);
-    tree_build(4*node-1,mid1+1,b1,a2,mid2);
-    tree_build(4*node+0,a1,mid2+1,mid1,b2);
-    tree_build(4*node+1,mid1+1,mid2+1,a2,b2);
+    build(4*node-2,a1,b1,mid1,mid2);
+    build(4*node-1,mid1+1,b1,a2,mid2);
+    build(4*node+0,a1,mid2+1,mid1,b2);
+    build(4*node+1,mid1+1,mid2+1,a2,b2);
 
     tree[node]=max(tree[4*node-2],max(tree[4*node-1],max(tree[4*node],tree[4*node+1])));
 
     return ;
 }
 
-int tree_query(int node,int a1,int b1, int a2, int b2, int x1,int y1, int x2, int y2)
+int query(int node,int a1,int b1, int a2, int b2, int x1,int y1, int x2, int y2)
 {
     if(a2<x1 || a1>x2 || y1>b2 || y2<b1 ||a1>a2 || b1>b2)
     {
@@ -85,18 +66,16 @@ int tree_query(int node,int a1,int b1, int a2, int b2, int x1,int y1, int x2, in
     int mid1 = (a1+a2)/2;
     int mid2 = (b1+b2)/2;
 
-    ret = max(ret,tree_query(4*node-2,a1,b1,mid1,mid2,x1,y1,x2,y2));
-    ret = max(ret,tree_query(4*node-1,mid1+1,b1,a2,mid2,x1,y1,x2,y2));
-    ret = max(ret,tree_query(4*node+0,a1,mid2+1,mid1,b2,x1,y1,x2,y2));
-    ret = max(ret,tree_query(4*node+1,mid1+1,mid2+1,a2,b2,x1,y1,x2,y2));
+    ret = max(ret,query(4*node-2,a1,b1,mid1,mid2,x1,y1,x2,y2));
+    ret = max(ret,query(4*node-1,mid1+1,b1,a2,mid2,x1,y1,x2,y2));
+    ret = max(ret,query(4*node+0,a1,mid2+1,mid1,b2,x1,y1,x2,y2));
+    ret = max(ret,query(4*node+1,mid1+1,mid2+1,a2,b2,x1,y1,x2,y2));
 
     return ret;
-
 }
 
 int main()
 {
-
     int test,i,j,Case,n,q,a1,b1,s;
     read(test);
     rep(Case,1,test+1)
@@ -113,8 +92,7 @@ int main()
                 read(arr[i][j]);
             }
         }
-
-        tree_build(1,1,1,n,n);
+        build(1,1,1,n,n);
 
         printf("Case %d:\n",Case);
 
@@ -124,12 +102,9 @@ int main()
             read(b1);
             read(s);
 
-            int ret = tree_query(1,1,1,n,n,a1,b1,a1+s-1,b1+s-1);
-
+            int ret = query(1,1,1,n,n,a1,b1,a1+s-1,b1+s-1);
             printf("%d\n",ret);
         }
-
     }
-
     return 0;
 }
